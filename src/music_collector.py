83cae35fda3aedccf54ea0a09b0b9ca59caa01a2
mgalushka <mgalushka@gmail.com>
2021-07-30 16:39:04 +0100

    Fetch database

--- src/music_collector.py
+++ src/music_collector.py
@@ -4,7 +4,7 @@ import requests
 
 DATABASE = '../db.imslp'
 
-URL = 'https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=1/start='
+URL = 'https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=2/start='
 MAX_PAGE = 1000
 
 if __name__ == '__main__':
@@ -12,8 +12,12 @@ if __name__ == '__main__':
     for start in range(0, MAX_PAGE):
         url = URL + str(start)
         response = requests.get(url)
-        if response.status_code != 400:
+        if response.status_code != 200:
             logging.error("End of stream, received error HTTP code: {}".format(response.status_code))
             break
         page = response.json()
-        for
+        with open(DATABASE, 'a') as file:
+            for key, value in page.items():
+                json_line = json.dumps(value)
+                file.write(json_line)
+                file.write('\n')
