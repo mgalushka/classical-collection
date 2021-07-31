2ba074ce7a75269a47efc14404f8db0d9fe911d8
mgalushka <mgalushka@gmail.com>
2021-07-31 10:59:21 +0100

    Fetching from DB and rendering on a web app. Great first step in my little web application\!

--- web/app.py
+++ web/app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+# import logging
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
+
+from src.database.db import DatabaseDriver
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html', name='Lolwhat???')
+def start():
+    return render_template('index.html', results=[])
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        return redirect('/')
+    app.logger.warning(request.args)
+    term = request.form.get('term', '')
+    app.logger.warning(term)
+    print(term)
+    database = DatabaseDriver()
+    results = []
+    try:
+        results = database.search(term)
+        app.logger.warning(results)
+    finally:
+        database.close()
+    return render_template('index.html', results=results)
